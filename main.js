0c58ea8f6c3e04afd3f3dc00d75c40ce7ced8c9e
miriam <miriammugure610@gmail.com>
2024-05-27 16:19:49 +0300

    add a fizzbuzz testcase to the repository

---
+++ main.js
@@ -0,0 +1,17 @@
+//fizz buzz
+module.exports = {numbers}
+function numbers(n){
+  if(n%3===0 && n%5===0){
+    return "FizzBuzz";
+  }else if (n%3===0){
+    return "Fizz";
+  }else if(n%5===0){
+    return "Buzz";
+  }else{
+    return n;
+  }
+}
+console.log(numbers(1000))
+  
+
+

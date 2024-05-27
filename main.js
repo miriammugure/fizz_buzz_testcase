4481b526c27f4a7837710df0a01084d774b9a06e
miriam <miriammugure610@gmail.com>
2024-05-27 16:40:37 +0300

    refactor code

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 //fizz buzz
-module.exports = {numbers}
-function numbers(n){
-  if(n%3===0 && n%5===0)return "FizzBuzz";
-  if (n%3===0)return "Fizz";
-  if(n%5===0)return "Buzz";
+module.exports = { numbers };
+function numbers(n) {
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n;
-  }
-console.log(numbers(1000))
-  
-
-
+}
+console.log(numbers(1000));
